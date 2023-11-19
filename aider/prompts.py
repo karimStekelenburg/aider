4feb7318a67e8e9e27159ccac1e4aa2d78fa45b1
Karim Stekelenburg <karim@animo.id>
2023-11-19 13:27:18 +0100

    feat: shorter commit message
    Signed-off-by: Karim Stekelenburg <karim@animo.id>

--- aider/prompts.py
+++ aider/prompts.py
@@ -7,6 +7,8 @@ Review the provided context and diffs which are about to be committed to a git r
 Generate a *SHORT* 1 line, 1 sentence commit message that describes the purpose of the changes.
 The commit message MUST be in the past tense.
 It must describe the changes *which have been made* in the diffs!
+Tiy *MUST* follow the conventional commits method. You summarize the changes in the body of the message.
+KEEP IT CONCISE
 Reply with JUST the commit message, without quotes, comments, questions, etc!
 """
 
